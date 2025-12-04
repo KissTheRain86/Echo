55bb8675462823efdb49188ec01e095bfd5c1faf
zengweihao <zwh86771385@gmail.com>
2025-12-05 01:38:30 +0800

    阻塞式交互

---
+++ EchoClient/Assets/Scripts/Echo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using UnityEngine.UI;
+
+public class Echo : MonoBehaviour
+{
+    Socket socket;
+    public InputField InputField;
+    public Text Text;
+
+    public void Connection()
+    {
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Connect("127.0.0.1", 8888);
+    }
+
+    public void Send()
+    {
+        string sendStr = InputField.text;
+        byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(sendStr);
+        socket.Send(sendBytes);
+
+        byte[] readBuff = new byte[1024];
+        int count = socket.Receive(readBuff);
+        string rescStr = System.Text.Encoding.UTF8.GetString(readBuff, 0, count);
+
+        Text.text = rescStr;
+        socket.Close();
+    }
+}
---
+++ EchoServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Socket listenfd = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            //bind
+            IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEdp = new IPEndPoint(ipAddr, 8888);
+            listenfd.Bind(ipEdp);
+            //listen
+            listenfd.Listen(0);
+            Console.WriteLine("Server Start");
+            while (true)
+            {
+                //Accecpt
+                Socket connfd = listenfd.Accept();
+                Console.WriteLine("Server Accept");
+                //Receive
+                byte[] readBuff = new byte[1024];
+                int count = connfd.Receive(readBuff);
+                string readStr = System.Text.Encoding.UTF8.GetString(readBuff, 0, count);
+                Console.WriteLine("Server Recieved " + readStr);
+                //Send
+                byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(readStr);
+                connfd.Send(sendBytes);
+            }
+        }
+    }
+
+}
